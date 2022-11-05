035ddb201c87caab9a526d0ca7ebba95708b60ff
Scott <skotters@gmail.com>
2022-11-05 13:04:20 -0400

    github repo test check

---
+++ KnowledgeCheck2/KnowledgeCheck2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KnowledgeCheck2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("github commit test............");
+        }
+    }
+}
