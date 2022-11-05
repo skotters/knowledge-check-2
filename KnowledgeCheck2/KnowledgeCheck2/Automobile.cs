806dcac8a18111705d0495ab5c33ae9029ac7d3a
Scott <skotters@gmail.com>
2022-11-05 13:50:02 -0400

    updates added

---
+++ KnowledgeCheck2/KnowledgeCheck2/Automobile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    internal class Automobile
+    {
+        public int Horsepower { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
--- KnowledgeCheck2/KnowledgeCheck2/Program.cs
+++ KnowledgeCheck2/KnowledgeCheck2/Program.cs
@@ -6,8 +6,33 @@ namespace KnowledgeCheck2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("github commit test............");
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Hatchback>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new Hatchback();
+
+                Console.WriteLine("Enter hatchback make:");
+                myClass.Make = Console.ReadLine();
+
+                Console.WriteLine("Enter hatchback model:");
+                myClass.Model = Console.ReadLine();
+
+                Console.WriteLine("Enter hatchback horsepower:");
+                myClass.Horsepower = Convert.ToInt32(Console.ReadLine());
+
+                recordList.Add(myClass);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach (var item in recordList)
+            {
+                Console.WriteLine($"The {item.Make} {item.Model} hatchback has {item.Horsepower} horsepower");
+            }
+
         }
     }
 }
